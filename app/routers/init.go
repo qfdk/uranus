6bd288dbfb784ccee7b26069ab080457908a4bb6
qfdk <qfdk2010@gmail.com>
2022-01-16 16:43:37 +0100

    fix: 调整路由位置

--- app/routers/init.go
+++ app/routers/init.go
@@ -9,16 +9,16 @@ import (
 
 // RegisterRoutes /** 路由组*/
 func RegisterRoutes(engine *gin.Engine) {
-	engine.Use(gin.BasicAuth(gin.Accounts{config.GetAppConfig().Username: config.GetAppConfig().Password}))
 	// 错误中间件
 	//engine.Use(middlewares.ErrorHttp)
 	// 静态文件路由
 	engine.StaticFS("/public", http.Dir("./web/public"))
 	// 初始化路由
+	engine.GET("/ws", ws)
+	engine.Use(gin.BasicAuth(gin.Accounts{config.GetAppConfig().Username: config.GetAppConfig().Password}))
 	engine.GET("/", controllers.Index)
 	engine.GET("/config", controllers.GetNginxCompileInfo)
 	engine.POST("/nginx", controllers.Nginx)
-	engine.GET("/ws", ws)
 	sitesRouter(engine)
 	sslRouter(engine)
 }
