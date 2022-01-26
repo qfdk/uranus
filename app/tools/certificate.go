72fc09f1f62c6328ef09d77e43c5f6cf095cf57e
qfdk <qfdk2010@gmail.com>
2022-01-26 08:52:22 +0100

    fix: 环境变量判断是否开启测试模式

--- app/tools/certificate.go
+++ app/tools/certificate.go
@@ -49,7 +49,7 @@ func IssueCert(domains []string) error {
 
 	config := lego.NewConfig(&myUser)
 
-	if gin.EnvGinMode != "release" {
+	if gin.ReleaseMode != "release" {
 		config.CADirURL = "https://acme-staging-v02.api.letsencrypt.org/directory"
 	}
 
