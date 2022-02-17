ea5f9cc2bf4ec43093fc4f05cadbd3a08e9f3c57
qfdk <qfdk2010@gmail.com>
2022-02-17 20:46:43 +0100

    fix: 没有 redis 开启 跳过证书检测

--- app/services/renewSSL.go
+++ app/services/renewSSL.go
@@ -31,23 +31,25 @@ func RenewSSL() {
 	spec := "5 0 * * *"
 	c := cron.New()
 	c.AddFunc(spec, func() {
-		keys, _ := RedisClient.Keys(RedisPrefix + "*").Result()
-		for _, key := range keys {
-			redisData, _ := RedisClient.Get(key).Result()
-			var need2Renew = false
-			var output RedisData
-			json.Unmarshal([]byte(redisData), &output)
-			if output.NotAfter.Unix() != -62135596800 {
-				if output.NotAfter.Sub(time.Now()) < time.Hour*24*30 {
-					log.Printf("%v => 需要续期\n", output.Domains)
-					need2Renew = true
-				} else {
-					log.Printf("%v => 证书续期时间: %v\n", output.Domains, output.NotAfter.Format("2006-01-02 15:04:05"))
+		if GetAppConfig().Redis {
+			keys, _ := RedisClient.Keys(RedisPrefix + "*").Result()
+			for _, key := range keys {
+				redisData, _ := RedisClient.Get(key).Result()
+				var need2Renew = false
+				var output RedisData
+				json.Unmarshal([]byte(redisData), &output)
+				if output.NotAfter.Unix() != -62135596800 {
+					if output.NotAfter.Sub(time.Now()) < time.Hour*24*30 {
+						log.Printf("%v => 需要续期\n", output.Domains)
+						need2Renew = true
+					} else {
+						log.Printf("%v => 证书续期时间: %v\n", output.Domains, output.NotAfter.Format("2006-01-02 15:04:05"))
+					}
 				}
-			}
-			if need2Renew {
-				IssueCert(strings.Split(output.Domains, ","), output.FileName)
-				ReloadNginx()
+				if need2Renew {
+					IssueCert(strings.Split(output.Domains, ","), output.FileName)
+					ReloadNginx()
+				}
 			}
 		}
 	})
