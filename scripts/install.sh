d6f8c8e75476c4656ede45254c6c0e1b49eadc10
qfdk <qfdk2010@gmail.com>
2022-03-26 17:18:35 +0100

    fix: 测试

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,95 @@
 #!/bin/bash
 export GIN_MODE=release
 APP_NAME=nginx-proxy-manager
-if [ ! -d ~/nginx-proxy-manager  ];then
-  mkdir -p ~/nginx-proxy-manager
-else
-  kill -9 $(ps aux|grep "nginx-proxy-manager"|grep -v grep|awk '{print $2}')
-  rm ~/nginx-proxy-manager/$APP_NAME
-fi
-cd ~/nginx-proxy-manager
-wget https://fr.qfdk.me/nginx-proxy-manager
-chmod +x $APP_NAME
-./$APP_NAME >app.log 2>&1 &
+ServicePath="/etc/systemd/system/${APP_NAME}.service"
+INSTALL_PATH="/etc/nginx-proxy-manager"
+
+FontGreen="\033[32m"
+FontRed="\033[31m"
+FontYellow="\033[33m"
+FontSuffix="\033[0m"
+
+install_service() {
+  cat >"$ServicePath" <<EOF
+[Unit]
+Description=Nginx proxy manager
+After=network.target
+[Service]
+Type=simple
+ExecStart=/etc/nginx-proxy-manager/$APP_NAME
+Restart=on-failure
+TimeoutStopSec=5
+KillMode=mixed
+[Install]
+WantedBy=multi-user.target
+EOF
+  chmod 644 "$ServicePath"
+  echo "info: Systemd service files have been installed successfully!"
+  systemctl daemon-reload
+  SYSTEMD='1'
+}
+
+start_service() {
+  if [[ -f ServicePath ]]; then
+    systemctl start ${APP_NAME}
+    sleep 1s
+    if systemctl -q is-active ${APP_NAME}; then
+      echo 'info: Start the Nginx proxy manager service.'
+    else
+      echo -e "${FontRed}error: Failed to start the Nginx proxy manager service.${FontSuffix}"
+      exit 1
+    fi
+  fi
+}
+
+stop_service() {
+  if ! systemctl stop ${APP_NAME}; then
+    echo -e "${FontRed}error: Failed to stop Nginx proxy manager service.${FontSuffix}"
+    exit 1
+  fi
+  echo "info: Nginx proxy manager service Stopped."
+}
+
+main() {
+
+  if systemctl list-unit-files | grep -qw ${APP_NAME}; then
+    if [[ -n "$(pidof ${APP_NAME})" ]]; then
+      stop_service
+      NGINX_PROXY_MANAGER='1'
+    fi
+  fi
+
+  if [ ! -d ${INSTALL_PATH} ]; then
+    mkdir -p ${INSTALL_PATH}
+  else
+    kill -9 $(pidof ${APP_NAME})
+    rm ${INSTALL_PATH}/${APP_NAME}
+  fi
+
+  cd ${INSTALL_PATH}
+  wget https://fr.qfdk.me/nginx-proxy-manager
+  chmod +x $APP_NAME
+
+  install_service
+  if [[ "$SYSTEMD" -eq '1' ]]; then
+    echo "installed: ${ServicePath}"
+  fi
+
+  if [[ "$NGINX_PROXY_MANAGER" -eq '1' ]]; then
+    systemctl start ${APP_NAME}
+  else
+    systemctl start ${APP_NAME}
+    systemctl enable ${APP_NAME}
+    sleep 1s
+
+    if systemctl -q is-active ${APP_NAME}; then
+      echo "info: Start and enable the Nginx proxy manager service."
+    else
+      echo -e "${FontYellow}warning: Failed to enable and start the Nginx proxy manager service.${FontSuffix}"
+    fi
+  fi
+  #  ./$APP_NAME >app.log 2>&1 &
+  install_service
+}
+
+main
