a90c9737d120212bdc4bf3523691450f700c1fda
qfdk <qfdk2010@gmail.com>
2022-04-15 11:42:40 +0200

    fix: 测试新版本

--- app/services/update.go
+++ app/services/update.go
@@ -35,8 +35,9 @@ func ToUpdateProgram(url string) {
 	}
 
 	if resp.StatusCode == http.StatusOK {
-		newProjectName := path.Join(config.GetAppConfig().InstallPath, projectName+"_new")
 		log.Printf("[INFO] 获取更新: [%s]", projectName)
+		_ = os.Rename(projectName, path.Join(config.GetAppConfig().InstallPath, projectName+"_back"))
+		newProjectName := path.Join(config.GetAppConfig().InstallPath, projectName)
 		downFile, err := os.Create(newProjectName)
 		checkIfError(err)
 		defer downFile.Close()
@@ -61,12 +62,9 @@ func ToUpdateProgram(url string) {
 			err = os.Remove(newProjectName)
 			checkIfError(err)
 		}
-
-		log.Printf("[INFO] [%s] 下载成功, 准备下一步操作", projectName)
 		_ = os.Chmod(newProjectName, os.ModePerm)
-		_ = os.Remove(projectName)
-		_ = os.Rename(newProjectName, path.Join(config.GetAppConfig().InstallPath, projectName))
-		//syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+		_ = os.Remove(projectName + "_back")
+		log.Printf("[INFO] [%s] 下载成功, 准备下一步操作", projectName)
 		syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
 	} else {
 		log.Printf("[ERROR] [%s]更新失败", projectName)
