b950bdc64deea29bd6f9a513953c8a6e7f09cb3c
qfdk <qfdk2010@gmail.com>
2022-04-22 09:53:44 +0200

    fix: 优化升级界面

--- app/services/nginx.go
+++ app/services/nginx.go
@@ -6,55 +6,77 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"syscall"
 	"uranus/app/config"
 )
+
+var sysType = runtime.GOOS
 
 func NginxStatus() string {
 	pidPath := config.ReadNginxCompileInfo().NginxPidPath
 	out, err := exec.Command("cat", pidPath).CombinedOutput()
 	var result = string(out)
 	if err != nil {
-		log.Println("PID 找不到")
-		log.Println(err)
+		log.Printf("[NGINX] 没有启动, %s", out)
 		result = "KO"
+	} else {
+		result = string(out)
 	}
 	return result
 }
 
 func StartNginx() string {
-	log.Printf("[PID][%d]: [Nginx] 启动", syscall.Getpid())
-	out, err := exec.Command("systemctl", "start", "nginx").CombinedOutput()
+	var out []byte
+	var err error
+	if sysType == "darwin" {
+		out, err = exec.Command("nginx").CombinedOutput()
+	} else {
+		out, err = exec.Command("systemctl", "start", "nginx").CombinedOutput()
+	}
 	var result = "OK"
 	if err != nil {
 		result = err.Error()
-		log.Println(result)
-		log.Println(out)
+		log.Printf("[PID][%d]: [NGINX] 启动失败, %s", syscall.Getpid(), out)
+	} else {
+		log.Printf("[PID][%d]: [NGINX] 启动成功", syscall.Getpid())
 	}
 	return result
 }
 
 func ReloadNginx() string {
-	log.Println("[Nginx] 重载配置文件")
+	log.Println("[NGINX] 重载配置文件")
 	var result = "OK"
 	if NginxStatus() != "KO" {
-		out, err := exec.Command("systemctl", "reload", "nginx").CombinedOutput()
+		var out []byte
+		var err error
+		if sysType == "darwin" {
+			out, err = exec.Command("nginx", "-s", "reload").CombinedOutput()
+		} else {
+			out, err = exec.Command("systemctl", "reload", "nginx").CombinedOutput()
+		}
 		if err != nil {
-			log.Println("[Nginx] 重载配置出现错误")
+			log.Println("[NGINX] 重载配置出现错误")
 			result = string(out)
 		}
 	} else {
-		log.Println("[Nginx] 没有启动,不用重载配置 !")
+		log.Println("[NGINX] 没有启动,不用重载配置 !")
 	}
 	return result
 }
 
 func StopNginx() string {
-	log.Printf("[PID][%d]: [Nginx] 停止", syscall.Getpid())
-	_, err := exec.Command("systemctl", "stop", "nginx").CombinedOutput()
+	log.Printf("[PID][%d]: [NGINX] 停止", syscall.Getpid())
+	var err error
+	if sysType == "darwin" {
+		_, err = exec.Command("nginx", "-s", "stop").CombinedOutput()
+	} else {
+		_, err = exec.Command("systemctl", "stop", "nginx").CombinedOutput()
+	}
+
 	var result = "OK"
 	if err != nil {
-		log.Println("[Nginx] 停止出现错误")
+		log.Printf("[PID][%d]: [NGINX] 停止出现错误", syscall.Getpid())
 		log.Println(err)
 		result = "KO"
 	}
@@ -71,6 +93,6 @@ func getNginxConfPath() string {
 func SaveNginxConf(content string) string {
 	path := filepath.Join(getNginxConfPath(), "nginx.conf")
 	ioutil.WriteFile(path, []byte(content), 0644)
-	log.Println("[Nginx] 保存配置成功")
+	log.Println("[NGINX] 保存配置成功")
 	return ReloadNginx()
 }
