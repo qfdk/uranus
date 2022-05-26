c0e5d3201262f2f14adb93c0bc6e5dc95d14454c
qfdk <qfdk2010@gmail.com>
2022-05-27 00:00:01 +0200

    fix : 修改terminal 样式

--- views/public/js/terminal.js
+++ views/public/js/terminal.js
@@ -38,6 +38,7 @@
         terminal.focus();
         setTimeout(function () {
             fitAddon.fit()
+            ws.send("export TERM=xterm\n")
             ws.send("PS1=\"\\[\\033[01;31m\\]\\u\\[\\033[01;33m\\]@\\[\\033[01;36m\\]\\h \\[\\033[01;33m\\]\\w \\[\\033[01;35m\\]\\$ \\[\\033[00m\\]\"\n")
             ws.send("alias ls='ls --color'\n")
             ws.send("alias ll='ls -alF'\n")
